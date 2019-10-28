e3471cb3be4c5209b4681c6461b45ab854c1384c
leos <leonardo.siri.email@gmail.com>
2019-10-28 17:48:04 +0100

    use isSameChar inside the while loop

--- palindrome/js/main.js
+++ palindrome/js/main.js
@@ -13,28 +13,21 @@ function isPalindrome(word)
     }
     // if the word is more than one character long
     else {
-        /* in case the word contains uppercase characters, the word is converted to
-        lower case ("abBA" is still palindrome) */
+        /* in case the word contains uppercase characters, the word is converted to lower case
+        ("abBA" is still palindrome) */
         word = word.toLowerCase();
         var i = 0;
         var j = word.length - 1;
-        // var isSameChar = true;
+        var isSameChar = true;
         var iterations = Math.floor(word.length / 2);
-        // while (i < iterations && isSameChar)
-        // {
-        //     /* if the characters are different the word is not palindrome (isSameChar
-        //     will be false) */
-        //     isSameChar = word.charAt(i) === word.charAt(j);
-        //     ++i;
-        //     --j;
-        // }
-        // return isSameChar;
-        while (i < iterations)
+        while (i < iterations && isSameChar)
         {
-            if (word.charAt(i) !== word.charAt(j)) return false;
+            /* if the characters are different the word is not palindrome (isSameChar will be
+            false) */
+            isSameChar = word.charAt(i) === word.charAt(j);
             ++i;
             --j;
         }
-        return true;
+        return isSameChar;
     }
 }
